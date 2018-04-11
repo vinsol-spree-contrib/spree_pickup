11a70b0280728198b753edc6c55d90e54ca0277b
Harsh Agrawal <harsh@vinsol.com>
2018-04-11 18:31:09 +0530

    Add validations

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -6,6 +6,16 @@ Spree::Order.class_eval do
 
   Spree::Order::SHIPMENT_STATES += %w(delivered)
 
+  _validators.delete(:shipment_state)
+
+  _validate_callbacks.each do |callback|
+    if callback.raw_filter.respond_to? :attributes
+      callback.raw_filter.attributes.delete :shipment_state
+    end
+  end
+
+  validates :shipment_state,  inclusion:  { in: Spree::Order::SHIPMENT_STATES, allow_blank: true }
+
   def pickup_address
     ship_address
   end
