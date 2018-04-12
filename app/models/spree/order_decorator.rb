9c2663566deb196d891b12d2b68dd01a9e0bab3d
Harsh Agrawal <harsh@vinsol.com>
2018-04-12 15:35:39 +0530

    Add shipment states in order

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -4,7 +4,17 @@ Spree::Order.class_eval do
 
   before_save :clone_pickup_address, if: :pickup_location_changed?
 
-  SHIPMENT_STATES += %w(delivered)
+  Spree::Order::SHIPMENT_STATES += %w(delivered)
+
+   _validators.delete(:shipment_state)
+
+  _validate_callbacks.each do |callback|
+    if callback.raw_filter.respond_to? :attributes
+      callback.raw_filter.attributes.delete :shipment_state
+    end
+  end
+
+  validates :shipment_state,  inclusion:  { in: Spree::Order::SHIPMENT_STATES, allow_blank: true }
 
   def pickup_address
     ship_address
